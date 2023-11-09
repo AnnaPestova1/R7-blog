7692d882f1b4e6caa0580fd6d6fea72946324f9e
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-11-09 10:57:31 -0500

    Creating an Associated Record

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+      # No `new` action because form is provided by PostsController#show
+  def create
+    @post = Post.find(params[:post_id])
+    # Create has many associated model, just like we did in the console before
+    @comment = @post.comments.create(comment_params)
+    # We want to show the comment in the context of the Post
+    redirect_to @post
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name, :content)
+  end
+end
